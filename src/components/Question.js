bb51bfe95cb19c241db1de2052428c10d2e37291
Mamdouh93Murad <mamdouh93morad@gmail.com>
2021-05-27 17:44:56 +0200

    Added Question Details Basic UI

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-
-export default class Question extends Component {
+import { connect } from 'react-redux'
+export class Question extends Component {
     render() {
+        const path = window.location.pathname
+        const url = path.split('/');
+        const id = url[2]
         return (
-            <div>
-                <h1>welcome to poll details</h1>
+            <div style={{textAlign:'center', justifyContent: 'space-around'}}>
+                <h1 >Poll Details</h1>
             </div>
         )
     }
 }
+// function mapStateToProps({authedUser, questions}) {
+//     return{
+
+//     }
+// }
+export default connect(/*mapStateToProps*/)(Question)
